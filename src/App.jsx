978f21a1b8aaca926bf256aec9862905ca83d93c
Leonel Aguirre <leonel.aguirre182@gmail.com>
2024-03-12 11:14:20 -0600

    :coffin: Codebase cleanup.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app__header">
+        <h1 className="text-3xl font-bold underline">Tailwing working!</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
