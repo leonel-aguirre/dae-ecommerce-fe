503840f3bd5594d7df781b2c2dffda53a73154eb
Leonel Aguirre <leonel.aguirre182@gmail.com>
2024-05-29 23:27:48 -0600

    DEFE-4 - Category filters working.

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -2,33 +2,47 @@ import "./SearchPage.scss"
 
 import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { useSearchParams } from "react-router-dom"
+import { useNavigate, useSearchParams } from "react-router-dom"
 
 import { productActions, productSelectors } from "../../state/index"
 import ProductCard from "../../components/ProductCard/ProductCard"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCircleNotch } from "@fortawesome/free-solid-svg-icons"
+import { faCircleNotch, faX } from "@fortawesome/free-solid-svg-icons"
 
-const { fetchSearchResults } = productActions
-const { selectSearchResults } = productSelectors
+const { fetchSearchResults, fetchProductCategories } = productActions
+const { selectSearchResults, selectProductCategories } = productSelectors
 
 const SearchPage = () => {
   const searchResults = useSelector(selectSearchResults)
+  const productCategories = useSelector(selectProductCategories)
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   const [params] = useSearchParams()
   const [isLoading, setIsLoading] = useState(false)
 
   const searchTerm = params.get("term")
+  const filter = params.get("filter")
 
   useEffect(() => {
     const triggerFetch = async () => {
       setIsLoading(true)
-      await dispatch(fetchSearchResults(searchTerm, null))
+      await dispatch(fetchProductCategories())
+      await dispatch(fetchSearchResults(searchTerm, filter))
       setIsLoading(false)
     }
 
     triggerFetch()
-  }, [searchTerm])
+  }, [searchTerm, filter])
+
+  const handleFilterButtonClick = (category) => {
+    const formattedCategory = category?.replaceAll("&", "-and-")
+
+    if (searchTerm) {
+      navigate(`/search?term=${searchTerm}&filter=${formattedCategory}`)
+    } else {
+      navigate(`/search?filter=${formattedCategory}`)
+    }
+  }
 
   const renderResults = () => {
     if (isLoading) {
@@ -43,18 +57,52 @@ const SearchPage = () => {
       )
     } else {
       return (
-        <section className="search-page__search-results-section">
-          {searchResults.map((product) => (
-            <ProductCard key={product.id} product={product} isSmall={true} />
-          ))}
-          {searchResults.length === 0 && (
-            <p className="search-page__no-results-text">
-              <span className="search-page__no-results-icon">😿</span>
-              Oops, no products found matching your search. Please try different
-              keywords.
-            </p>
-          )}
-        </section>
+        <>
+          <section className="search-page__search-filters-section">
+            <ul className="search-page__filters-list">
+              {productCategories.map((productCategory, index) => {
+                const { id, title } = productCategory
+                const currentFilter = filter?.replaceAll("-and-", "&") || ""
+
+                return (
+                  <li key={id} className="search-page__filters-list-item">
+                    <button
+                      disabled={title === currentFilter}
+                      className="search-page__filter-button"
+                      onClick={() => handleFilterButtonClick(title)}
+                    >
+                      {title}
+                    </button>
+                  </li>
+                )
+              })}
+              <li>
+                <button
+                  className="search-page__filter-button is-clear-button"
+                  onClick={() =>
+                    navigate(
+                      `/search${searchTerm ? "?term=" + searchTerm : ""}`,
+                    )
+                  }
+                >
+                  <FontAwesomeIcon icon={faX} />
+                </button>
+              </li>
+            </ul>
+          </section>
+          <section className="search-page__search-results-section">
+            {searchResults.map((product) => (
+              <ProductCard key={product.id} product={product} isSmall={true} />
+            ))}
+            {searchResults.length === 0 && (
+              <p className="search-page__no-results-text">
+                <span className="search-page__no-results-icon">😿</span>
+                Oops, no products found matching your search. Please try
+                different keywords.
+              </p>
+            )}
+          </section>
+        </>
       )
     }
   }
